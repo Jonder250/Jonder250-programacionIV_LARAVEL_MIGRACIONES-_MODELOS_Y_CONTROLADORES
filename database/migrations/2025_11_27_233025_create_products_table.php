ac7e498dfa6c0648f2d3b5760650035ffd3834c2
Jonder <jondercarrero25@gmail.com>
2025-11-27 18:04:59 -0600

    Actualizar Migration

--- database/migrations/2025_11_27_233025_create_products_table.php
+++ database/migrations/2025_11_27_233025_create_products_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('nombre');
+        $table->text('descripcion')->nullable();
+        $table->decimal('precio', 8, 2);
+        $table->unsignedInteger('stock')->default(0);
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
